e9b3b14cbfba8396d5239f0133f9129f60ca6c66
pman <pman-$-432894328923@gmail.com>
2024-08-04 12:18:53 +0200

    chore: update file(s): providers.tf

--- providers.tf
+++ providers.tf
@@ -1,22 +1,3 @@
-terraform {
-  required_version = ">= 1.7.0"
-
-  required_providers {
-    flux = {
-      source  = "fluxcd/flux"
-      version = ">= 1.2"
-    }
-    github = {
-      source  = "integrations/github"
-      version = ">= 6.1"
-    }
-    kind = {
-      source  = "tehcyx/kind"
-      version = ">= 0.5.1"
-    }
-  }
-}
-
 provider "flux" {
   kubernetes = {
     host                   = kind_cluster.this.endpoint
@@ -25,7 +6,7 @@ provider "flux" {
     cluster_ca_certificate = kind_cluster.this.cluster_ca_certificate
   }
   git = {
-    url = "https://github.com/${var.github_owner}/${var.github_repository}.git"
+    url = "https://github.com/${var.github_org}/${var.github_repository}.git"
     http = {
       username = "git" # This can be any string when using a personal access token
       password = var.github_token
@@ -34,7 +15,7 @@ provider "flux" {
 }
 
 provider "github" {
-  owner = var.github_owner
+  owner = var.github_org
   token = var.github_token
 }
 
