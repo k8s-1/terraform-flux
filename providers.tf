32b193bdcc3aff929c798164245cac411b4417a2
pman <pman-$-432894328923@gmail.com>
2024-08-04 11:33:59 +0200

    chore: update file(s): providers.tf

--- providers.tf
+++ providers.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_version = ">= 1.7.0"
+
+  required_providers {
+    flux = {
+      source  = "fluxcd/flux"
+      version = ">= 1.2"
+    }
+    github = {
+      source  = "integrations/github"
+      version = ">= 6.1"
+    }
+    kind = {
+      source  = "tehcyx/kind"
+      version = ">= 0.51"
+    }
+  }
+}
+
 provider "flux" {
   kubernetes = {
     host                   = kind_cluster.this.endpoint
