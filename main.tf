6265d01b9bc694c19433641f3017275b8c2d2934
pman <pman-$-432894328923@gmail.com>
2024-08-04 12:19:37 +0200

    chore: update file(s): main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,26 @@
+terraform {
+  required_version = ">= 1.7.0"
+
+  required_providers {
+    flux = {
+      source  = "fluxcd/flux"
+      version = ">= 1.2"
+    }
+    github = {
+      source  = "integrations/github"
+      version = ">= 6.1"
+    }
+    kind = {
+      source  = "tehcyx/kind"
+      version = ">= 0.4"
+    }
+  }
+}
+
 # ==========================================
 # Construct KinD cluster
 # ==========================================
 
-# resource <resource-type> <resource-name>
 resource "kind_cluster" "this" {
   name = "flux-e2e"
 }
