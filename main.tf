37276842a6c7bc8c86d40222f7ab6b501f888c3d
k8s-1 <176627561+k8s-1@users.noreply.github.com>
2024-08-04 12:54:12 +0200

    chore: update file(s): main.tf

---
+++ main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_version = ">= 1.7.0"
+
+  required_providers {
+    flux = {
+      source  = "fluxcd/flux"
+      version = ">= 1.2"
+    }
+    github = {
+      source  = "integrations/github"
+      version = ">= 6.1"
+    }
+    kind = {
+      source  = "tehcyx/kind"
+      version = ">= 0.4"
+    }
+  }
+}
+
+# ==========================================
+# Construct KinD cluster
+# ==========================================
+
+resource "kind_cluster" "this" {
+  name = "flux-e2e"
+}
+
+# ==========================================
+# Initialise a Github project - only useful
+# if used for e2e tests (unless you want to tf destroy
+# your entire repo every time)
+# Else, initialize the repo via github directly
+
+# ==========================================
+
+#resource "github_repository" "this" {
+#  name        = var.github_repository
+#  description = var.github_repository
+#  visibility  = "private"
+#  auto_init   = true # This is extremely important as flux_bootstrap_git will not work without a repository that has been initialised
+#}
+
+# ==========================================
+# Bootstrap KinD cluster
+# ==========================================
+
+resource "flux_bootstrap_git" "this" {
+  # depends_on = [github_repository.this]
+
+  path               = "clusters/my-cluster"
+}
