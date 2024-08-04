75cb0f3ad55e60558e7255a3145c69e41b511e19
pman <pman-$-432894328923@gmail.com>
2024-08-04 06:13:15 +0200

    chore: update file(s): providers.tf

--- providers.tf
+++ providers.tf
@@ -6,7 +6,7 @@ provider "flux" {
     cluster_ca_certificate = kind_cluster.this.cluster_ca_certificate
   }
   git = {
-    url = "https://github.com/${var.github_org}/${var.github_repo}.git"
+    url = "https://github.com/${var.github_org}/${var.github_repository}.git"
     http = {
       username = "git" # This can be any string when using a personal access token
       password = var.github_token
