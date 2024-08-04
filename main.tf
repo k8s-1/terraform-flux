3fc937ced73c4356bc21660a84f1385d00a51df9
pman <pman-$-432894328923@gmail.com>
2024-08-04 12:43:38 +0200

    chore: update file(s): main.tf

--- main.tf
+++ main.tf
@@ -29,12 +29,12 @@ resource "kind_cluster" "this" {
 # Initialise a Github project
 # ==========================================
 
-resource "github_repository" "this" {
-  name        = var.github_repository
-  description = var.github_repository
-  visibility  = "private"
-  auto_init   = true # This is extremely important as flux_bootstrap_git will not work without a repository that has been initialised
-}
+#resource "github_repository" "this" {
+#  name        = var.github_repository
+#  description = var.github_repository
+#  visibility  = "private"
+#  auto_init   = true # This is extremely important as flux_bootstrap_git will not work without a repository that has been initialised
+#}
 
 # ==========================================
 # Bootstrap KinD cluster
