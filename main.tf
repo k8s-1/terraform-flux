76b45e4591035e67eaca4dbe33ad64e71d0df955
pman <pman-$-432894328923@gmail.com>
2024-08-04 12:39:08 +0200

    chore: update file(s): main.tf

--- main.tf
+++ main.tf
@@ -43,6 +43,5 @@ resource "github_repository" "this" {
 resource "flux_bootstrap_git" "this" {
   depends_on = [github_repository.this]
 
-  embedded_manifests = true
   path               = "clusters/my-cluster"
 }
